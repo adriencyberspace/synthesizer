3d3830a3b141d2e4afa07a19dadc0daa78edc60f
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-02-21 13:24:05 -0600

    Changed keyboard size and added octave up button

--- music.js
+++ music.js
@@ -1,47 +1,70 @@
 var synth = new Tone.PolySynth().toDestination();
-  var notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+var notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
-  var html = "";
-  for (var octave = 0; octave < 2; octave++) {
+var html = "";
+var octave = 4;
 
-    for (var i = 0; i < notes.length; i++) {
-      var hasSharp = true;
-      var note = notes[i];
+buildKeyboard(octave);
 
-      if (note == 'E' || note == 'B') {
-        hasSharp = false;
-      }
-
-      html += `<div class="whitenote" 
-        onmousedown="noteDown(this, false)" 
-        onmouseup="noteUp(this, false)" 
-        onmouseleave="noteUp(this, false)" 
-        data-note="${note + (octave + 4)}">`;
-
-      if (hasSharp) {
-        html += `<div class="blacknote" 
-          onmousedown="noteDown(this, true)" 
-          onmouseup="noteUp(this, true)" 
-          onmouseleave="noteUp(this, true)" 
-          data-note="${note + '#' + (octave+4)}"></div>`;
-      }
-
-      html += '</div>';
-    }
-
+document.getElementById("button").onclick = () => {
+  octaveUp();
+  alert(octave);
 }
 
+function octaveUp() {
+  octave += 1;
+  buildKeyboard(octave);
+}
+
+function buildKeyboard(octave) {
+  html = "";
+  addKeys(notes.length, octave);
+  addKeys(4, octave + 1);
   document.querySelector('#keys-container').innerHTML = html;
+}
 
-  function noteUp(e, isSharp) {
-    e.style.background = isSharp ? 'black' : 'white';
-  }
 
-  function noteDown(e, isSharp) {
-    var note = e.dataset.note;
-    e.style.background = isSharp ? '#c29417' : '#fff6de';
-    synth.triggerAttackRelease(note, "16n");
-    event.stopPropagation();
-  }
+function addKeys(x, octave){
+  for (var i = 0; i < x; i++) {
+    var displaySharp = true;
+    var note = notes[i];
 
+    if (x == 4) {
+      if (note == 'F') {
+        displaySharp = false;
+      }
+    }
   
+    if (note == 'E' || note == 'B') {
+      displaySharp = false;
+    }
+  
+    html += `<div class="whitenote" 
+      onmousedown="noteDown(this, false)" 
+      onmouseup="noteUp(this, false)" 
+      onmouseleave="noteUp(this, false)" 
+      data-note="${note + (octave)}">`;
+  
+    if (displaySharp) {
+      html += `<div class="blacknote" 
+        onmousedown="noteDown(this, true)" 
+        onmouseup="noteUp(this, true)" 
+        onmouseleave="noteUp(this, true)" 
+        data-note="${note + '#' + (octave)}"></div>`;
+    }
+  
+    html += '</div>';
+  }
+}
+
+function noteUp(e, isSharp) {
+  e.style.background = isSharp ? 'black' : 'white';
+}
+
+function noteDown(e, isSharp) {
+  var note = e.dataset.note;
+  e.style.background = isSharp ? '#c29417' : '#fff6de';
+  synth.triggerAttackRelease(note, "16n");
+  event.stopPropagation();
+}
+
